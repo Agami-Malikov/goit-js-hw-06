14f8a96627ad7a3ffe94fa4ff5f2ac2ff4e0045f
Agami-Malikov <agami.malikov@gmail.com>
2022-07-05 20:00:09 +0300

    3 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsGall = document.querySelector("#ingredients");
+
+const makeGall = (ingredients) => {
+  const gallEl = document.createElement("li");
+  gallEl.classList.add("item");
+  gallEl.textContent = ingredients;
+  return gallEl;
+};
+
+const items = ingredients.map(makeGall);
+
+ingredientsGall.append(...items);
