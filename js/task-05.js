8a801252f608b3aba120af9e440e4a17ea17c745
Agami-Malikov <agami.malikov@gmail.com>
2022-07-11 14:53:08 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inp = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 inp.addEventListener("input", () => {
-  if (inp.value.length <= 0) {
+  if (inp.value.length === 0) {
     nameOutput.textContent = "Anonymous";
   } else {
     nameOutput.textContent = inp.value;
