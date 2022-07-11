b02fef13e11480aef9915cfc702b54ed1cefa1b3
Agami-Malikov <agami.malikov@gmail.com>
2022-07-11 17:44:53 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,12 @@
 const inp = document.querySelector("#validation-input");
 
 inp.addEventListener("blur", () => {
-  if (inp.value.length < inp.dataset.length) {
-    inp.classList.add("invalid");
-  } else {
+  if (inp.value.length === Number(inp.dataset.length)) {
     inp.classList.remove("invalid");
     inp.classList.add("valid");
+  } else {
+    inp.classList.add("invalid");
+    inp.classList.remove("valid");
   }
 });
 
